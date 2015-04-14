5edfa2bcec6fd749fb96fb24c039214951042f47
Endymion Kasanardjo <zetafish@gmail.com>
2015-04-14 15:39:58 +0200

    week1

--- week1.hs
+++ week1.hs
@@ -1,5 +1,4 @@
-module Week1 where
-
+-- Exercise 1
 toDigits    :: Integer -> [Integer]
 toDigitsRev :: Integer -> [Integer]
 
@@ -13,4 +12,24 @@ toDigitsRev n
  | n < 10    = [n]
  | otherwise = mod n 10 : toDigitsRev (div n 10)
 
+-- Exercise 2
+dub :: [Integer] -> [Integer]
+dub []         = []
+dub (x:[])     = [x]
+dub (x:(y:zs)) = [x,(y*2)] ++ dub zs
 
+doubleEveryOther xs = reverse (dub (reverse xs))
+
+-- Exercise 3
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
+
+-- Exercise 4
+validate :: Integer -> Bool
+validate n = 0 == (sumDigits (doubleEveryOther (toDigits n))) `mod` 10
+
+-- Exercise 5
+type Peg  = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> Move
