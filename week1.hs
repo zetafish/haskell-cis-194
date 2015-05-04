7a2b685e08df754cc38b36ac3c27e8c2a6c9933a
Endymion Kasanardjo <zetafish@gmail.com>
2015-05-04 23:50:54 +0200

    week1 : hanoi

--- week1.hs
+++ week1.hs
@@ -32,4 +32,7 @@ validate n = 0 == (sumDigits (doubleEveryOther (toDigits n))) `mod` 10
 -- Exercise 5
 type Peg  = String
 type Move = (Peg, Peg)
-hanoi :: Integer -> Peg -> Peg -> Peg -> Move
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi 1 a b c = [(a, b)]
+hanoi n a b c = (hanoi (n-1) a c b) ++ [(a,b)] ++ (hanoi (n-1) c b a)
